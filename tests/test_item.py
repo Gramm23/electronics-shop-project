6a1228d0f481b67633362745572516f566c5edd4
gramm23 <serglapinofff@gmail.com>
2023-08-05 14:19:35 +0300

    Initial code

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+import pytest
+
+
+@pytest.fixture
+def item_class():
+    return Item('Смартфон', 12.5, 15)
+
+
+def test_calculate_total_price(item_class):
+    assert item_class.calculate_total_price() == 187.5
+
+
+def test_apply_discount(item_class):
+    item_class.pay_rate = 0.5
+    item_class.apply_discount()
+    assert item_class.price == 6.25
