3b1253e81d8d145869786e22c7c0773ccb6b0b6b
gramm23 <serglapinofff@gmail.com>
2023-08-07 13:53:28 +0300

    add tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,5 +1,6 @@
 from src.item import Item
 import pytest
+from unittest.mock import patch, mock_open
 
 
 @pytest.fixture
@@ -15,3 +16,24 @@ def test_apply_discount(item_class):
     item_class.pay_rate = 0.5
     item_class.apply_discount()
     assert item_class.price == 6.25
+
+
+def test_name_setter():
+    item = Item('LongNameProduct', 10.0, 5)
+    item.name = "LongNameProduct"
+    assert item.name == 'LongNamePr'
+
+    item.name = 'ShortName'
+    assert item.name == 'ShortName'
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv()  # создание объектов из данных файла
+    assert len(Item.all) == 5
+
+
+def test_string_to_number():
+    assert Item.string_to_number("123") == 123
+    assert Item.string_to_number("3.14") == 3
+    assert Item.string_to_number("42.0") == 42
+
