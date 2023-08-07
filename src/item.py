9f3019400c476865f1567536f7b2712ff9fc7b97
gramm23 <serglapinofff@gmail.com>
2023-08-07 13:53:44 +0300

    add code

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +16,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
@@ -32,3 +35,39 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, data_name):
+        if len(data_name) > 10:
+            self.__name = data_name[0:10]
+        else:
+            self.__name = data_name
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        item_list = []
+        with open('../src/items.csv', newline='', encoding='cp1251') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                name = row['name']
+                price = float(row['price'])
+                quantity = int(row['quantity'])
+                item = cls(name, price, quantity)
+                item_list.append(item)
+
+        cls.all = item_list
+        return item_list
+
+    @staticmethod
+    def string_to_number(value: str):
+        return int(float(value))
+
+
+
+
+
+
