d60e57d39eb6e89ba7ea4517ecddbc5f0c480a0b
gramm23 <serglapinofff@gmail.com>
2023-08-08 15:39:30 +0300

    add repr and str

--- src/item.py
+++ src/item.py
@@ -22,6 +22,12 @@ class Item:
 
         Item.all.append(self)
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return self.__name
+
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
@@ -65,9 +71,6 @@ class Item:
     @staticmethod
     def string_to_number(value: str):
         return int(float(value))
-
-
-
 
 
 
